b0c9d0156a5a4765c69515a6ae3debd82bea826f
Atakan <komurcu508@gmail.com>
2022-07-05 15:02:35 +0300

    Initial Commit

---
+++ PersonalLinks/PersonalLinksApp.swift
@@ -0,0 +1,17 @@
+//
+//  PersonalLinksApp.swift
+//  PersonalLinks
+//
+//  Created by Atakan Kömürcü on 5.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct PersonalLinksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
